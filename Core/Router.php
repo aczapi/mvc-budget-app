62e970a8611aeaf4bfd3558f549d8b2f9376f70a
aczapi <agnieszka.czapiewska.programista@gmail.com>
2021-07-16 23:47:25 +0200

    add router

---
+++ Core/Router.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Router
+ * 
+ * PHP version 8.0.3
+ */
+
+class Router
+{
+}
---
+++ public/index.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Front controller
+ * 
+ * PHP version 8.0.3
+ */
+
+/**
+ * Routing
+ */
+require '../Core/Router.php';
+
+$router = new Router();
