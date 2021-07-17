8e1a7825a168a8a8c053ab24978143b06b8120ca
aczapi <agnieszka.czapiewska.programista@gmail.com>
2021-07-17 11:10:54 +0200

    add Home controller

---
+++ App/Controllers/Home.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controllers;
+
+/**
+ * Home controller
+ *
+ */
+class Home
+{
+  /**
+   * Show the index page
+   *
+   * @return void
+   */
+  public function index()
+  {
+    echo 'Home controller!';
+  }
+}
