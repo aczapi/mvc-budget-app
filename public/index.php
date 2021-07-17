59adbdbf137ebdeaf62e33a1078c979f1d74d93a
aczapi <agnieszka.czapiewska.programista@gmail.com>
2021-07-17 11:10:23 +0200

    add autoloader

--- public/index.php
+++ public/index.php
@@ -7,24 +7,26 @@
  */
 
 /**
+ * Autoloader
+ */
+spl_autoload_register(function ($class) {
+  $root = dirname(__DIR__);   // get the parent directory
+  $file = $root . '/' . str_replace('\\', '/', $class) . '.php';
+  if (is_readable($file)) {
+    require $root . '/' . str_replace('\\', '/', $class) . '.php';
+  }
+});
+
+/**
  * Routing
  */
 require '../Core/Router.php';
 
-$router = new Router();
+$router = new Core\Router();
 
-// $router->add('', ['controller' => 'Home', 'action' => 'index']);
-
+$router->add('', ['controller' => 'Home', 'action' => 'index']);
 $router->add('{controller}/{action}');
 $router->add('{controller}/{id:\d+}/{action}');
 
 // Match the requested route
-$url = $_SERVER['QUERY_STRING'];
-
-if ($router->match($url)) {
-  echo '<pre>';
-  var_dump($router->getParams());
-  echo '</pre>';
-} else {
-  echo "No route found for URL '$url'";
-}
+$router->dispatch($_SERVER['QUERY_STRING']);
