e9f6d02860556586ac36eca389482e321e6cfd17
aczapi <agnieszka.czapiewska.programista@gmail.com>
2021-07-17 08:48:50 +0200

    match requested route to the list of routes in the routing table

--- Core/Router.php
+++ Core/Router.php
@@ -13,6 +13,20 @@ class Router
    */
   protected $routes = [];
 
+  /**
+   * Parameters from the matched route
+   * @var array
+   */
+  protected $params = [];
+
+  /**
+   * Add a route to the routing table
+   *
+   * @param string $route  The route URL
+   * @param array  $params Parameters (controller, action, etc.)
+   *
+   * @return void
+   */
   public function add($route, $params)
   {
     $this->routes[$route] = $params;
@@ -25,5 +39,35 @@ class Router
   public function getRoutes()
   {
     return $this->routes;
+  }
+
+  /**
+   * Match the route to the routes in the routing table, setting the $params
+   * property if a route is found.
+   *
+   * @param string $url The route URL
+   *
+   * @return boolean  true if a match found, false otherwise
+   */
+
+  public function match($url)
+  {
+    foreach ($this->routes as $route => $params) {
+      if ($url == $route) {
+        $this->params = $params;
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Get the currently matched parameters
+   * 
+   * @return array
+   */
+  public function getParams()
+  {
+    return $this->params;
   }
 }
--- public/index.php
+++ public/index.php
@@ -16,7 +16,13 @@ $router = new Router();
 $router->add('', ['controller' => 'Home', 'action' => 'index']);
 
 
-// Display the routing table
-echo '<pre>';
-var_dump($router->getRoutes());
-echo '</pre>';
+// Match the requested route
+$url = $_SERVER['QUERY_STRING'];
+
+if ($router->match($url)) {
+  echo '<pre>';
+  var_dump($router->getParams());
+  echo '</pre>';
+} else {
+  echo "No route found for URL '$url'";
+}
