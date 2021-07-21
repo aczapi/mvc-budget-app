0dbcd98854b922c2b540566c32698284c69a8948
aczapi <agnieszka.czapiewska.programista@gmail.com>
2021-07-21 19:03:34 +0200

    find by email in db

--- App/Controllers/Login.php
+++ App/Controllers/Login.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use \Core\View;
+use \App\Models\User;
 
 /**
  * Login controller
@@ -18,5 +19,16 @@ class Login extends \Core\Controller
   public function newAction()
   {
     View::renderTemplate('Login/new.html');
+  }
+
+  /**Log in new user
+   * 
+   * @return void
+   */
+  public function createAction()
+  {
+    $user = User::findByEmail($_POST['email']);
+
+    var_dump($user);
   }
 }
--- App/Models/User.php
+++ App/Models/User.php
@@ -107,14 +107,27 @@ class User extends \Core\Model
    */
   public static function emailExists($email)
   {
+    return static::findByEmail($email) !== false;
+  }
+  /**
+   * Find a user model by email address
+   *
+   * @param string $email email address to search for
+   *
+   * @return mixed User object if found, false otherwise
+   */
+  public static function findByEmail($email)
+  {
     $sql = 'SELECT * FROM users WHERE email = :email';
 
     $db = static::getDB();
     $stmt = $db->prepare($sql);
-    $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+    $stmt->bindValue(':email', $email, PDO::PARAM_STR);
+
+    $stmt->setFetchMode(PDO::FETCH_CLASS, get_called_class());
 
     $stmt->execute();
 
-    return $stmt->fetch() !== false;
+    return $stmt->fetch();
   }
 }
