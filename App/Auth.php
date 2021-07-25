e72af6174ebb5ab33c581024acdf9deca06ae779
aczapi <agnieszka.czapiewska.programista@gmail.com>
2021-07-25 23:39:30 +0200

    set cookie

--- App/Auth.php
+++ App/Auth.php
@@ -16,10 +16,16 @@ class Auth
    * 
    * @return void
    */
-  public static function login($user)
+  public static function login($user, $remember_me)
   {
     session_regenerate_id(true);
     $_SESSION['user_id'] = $user->id;
+
+    if ($remember_me) {
+      if ($user->rememberLogin()) {
+        setcookie('remember_me', $user->remember_token, $user->expiry_timestamp, '/');
+      }
+    }
   }
 
   /** Logout the user
--- App/Controllers/Login.php
+++ App/Controllers/Login.php
@@ -35,14 +35,17 @@ class Login extends \Core\Controller
 
     if ($user) {
 
-      Auth::login($user);
+      Auth::login($user, $remember_me);
       Flash::addMessage('Login successful');
 
       $this->redirect(Auth::getReturnToPage());
     } else {
 
       Flash::addMessage('Login unsuccessful, please try again', Flash::WARNING);
-      View::renderTemplate('Login/new.html', ['email' => $_POST['email'], 'remember_me' => $remember_me]);
+      View::renderTemplate('Login/new.html', [
+        'email' => $_POST['email'],
+        'remember_me' => $remember_me
+      ]);
     }
   }
   public function destroyAction()
