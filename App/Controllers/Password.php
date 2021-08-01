120d23faefa254c395c3685f28c0f68b24b34799
aczapi <agnieszka.czapiewska.programista@gmail.com>
2021-08-01 22:09:05 +0200

    add reset_requested page

--- App/Controllers/Password.php
+++ App/Controllers/Password.php
@@ -3,6 +3,8 @@
 namespace App\Controllers;
 
 use \Core\View;
+use \App\Models\User;
+use \App\Flash;
 
 /**
  * Password controller
@@ -20,5 +22,17 @@ class Password extends \Core\Controller
   public function forgotAction()
   {
     View::renderTemplate('Password/forgot.html');
+  }
+  /**
+   * Send the password reset link to the supplied email
+   * 
+   * @return void
+   */
+  public function requestResetAction()
+  {
+    User::sendPasswordReset($_POST['email']);
+
+    Flash::addMessage('Please check your email');
+    View::renderTemplate('Password/reset_requested.html');
   }
 }
