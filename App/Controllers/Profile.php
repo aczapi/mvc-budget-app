013d0c418aa8b400517d26826148ab18d4d20947
aczapi <agnieszka.czapiewska.programista@gmail.com>
2021-08-06 20:08:24 +0200

    add Profile controller

---
+++ App/Controllers/Profile.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controllers;
+
+use \Core\View;
+
+/**
+ * Profile controller 
+ * 
+ */
+
+class Profile extends Authenticated
+{
+  /**
+   * Show the profile
+   * 
+   * @return void
+   */
+
+  public function showAction()
+  {
+    View::renderTemplate('Profile/show.html');
+  }
+}
